42e99a91b893982461176cb66a81a08f54add540
xiami <gaohlsilence@163.com>
2023-03-17 15:26:46 +0800

    feat(heart-cheak):修改心跳检测

--- najapy/common/process.py
+++ najapy/common/process.py
@@ -1,4 +1,3 @@
-import os
 from multiprocessing.shared_memory import SharedMemory
 
 from najapy.common.async_base import Utils
@@ -6,12 +5,14 @@ from najapy.common.base import ContextManager
 from najapy.common.struct import ByteArrayAbstract
 
 
-class SharedByteArray(SharedMemory, ByteArrayAbstract, ContextManager):
+class SharedByteArray(ByteArrayAbstract, ContextManager):
 
     def __init__(self, name=None, create=False, size=0):
 
-        SharedMemory.__init__(self, name, create, size)
         ByteArrayAbstract.__init__(self)
+
+        self._shared_memory = SharedMemory(name, create, size)
+        self._create_mode = create
 
     def _context_release(self):
 
@@ -19,24 +20,23 @@ class SharedByteArray(SharedMemory, ByteArrayAbstract, ContextManager):
 
     def release(self):
 
-        self.close()
+        self._shared_memory.close()
 
-        if (self._flags & os.O_CREAT) != 0:
-            self.unlink()
+        if self._create_mode:
+            self._shared_memory.unlink()
 
     def read(self, size):
 
-        return self._buf[:size]
+        return self._shared_memory.buf[:size]
 
     def write(self, buffer):
 
-        self._buf[:len(buffer)] = buffer
+        self._shared_memory.buf[:len(buffer)] = buffer
 
 
 class HeartbeatChecker(ContextManager):
 
     def __init__(self, name=r'default', timeout=60):
-
         self._name = f'heartbeat_{name}'
 
         self._timeout = timeout
@@ -47,12 +47,10 @@ class HeartbeatChecker(ContextManager):
             self._byte_array = SharedByteArray(self._name)
 
     def _context_release(self):
-
         self.release()
 
     @property
     def refresh_time(self):
-
         if self._byte_array is not None:
             return self._byte_array.read_unsigned_int()
         else:
@@ -65,10 +63,8 @@ class HeartbeatChecker(ContextManager):
             self._byte_array = None
 
     def check(self):
-        return True
-        # return (Utils.timestamp() - self.refresh_time) < self._timeout
+        return (Utils.timestamp() - self.refresh_time) < self._timeout
 
     def refresh(self):
-
         if self._byte_array is not None:
             self._byte_array.write_unsigned_int(Utils.timestamp())
